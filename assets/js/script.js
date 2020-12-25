7727139c8e8a6cc4969f30cb4c437f64382cec54
Shekhar Giri <gshekhar104@outlook.com>
2020-12-25 15:54:01 +0530

    PWA Complaint

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,3 +61,9 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(reg => console.log('service worker registered'))
+        .catch(err => console.log('service worker not registered', err));
+}
